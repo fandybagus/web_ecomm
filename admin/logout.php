40b00d5777ca51d7bc59cceaa75ee979db3080f5
fandybagus <163971537+fandybagus@users.noreply.github.com>
2024-04-29 17:40:43 +0700

    Add files via upload

--- admin/logout.php
+++ admin/logout.php
@@ -1,4 +1,8 @@
 <?php 
+session_start();
 
+// Hapus variabel sesi tertentu
+unset($_SESSION['userlogin']);
+unset($_SESSION['loginname']);
 echo "<meta http-equiv=\"REFRESH\" content=\"0;url=index.php\">";
 ?>
